8c18364c3ab88690e57d4a566bd2680a2e2cca7e
plusultradesarrollo <plusultradesarrollo@gmail.com>
2017-05-23 15:13:36 -0430

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: "#app",
+    data: {
+        name: "",
+        gender: 'f'
+    },
+    methods: {
+    	signUp: function(event) {
+
+    		alert('Signing up..')
+    	}
+    }
+});
